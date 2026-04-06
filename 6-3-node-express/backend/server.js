83b65c1d3270225045941d2e0f4d5304186198c0
TariqAlothman <s202274140@kfupm.edu.sa>
2026-04-06 08:43:18 +0300

    TODO 3

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,5 +1,8 @@
 // TODO 1: Import and Create express app instance
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { getRandomQuote } from "./quotes.js";
 
 const app = express();
 
@@ -8,15 +11,23 @@ const PORT = process.env.PORT || 3000;
 
 
 // TODO 4: Import and Apply CORS middleware
-
+app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
-
+app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
-
+app.get("/", (req, res) => {
+  res.send("Backend server is running");
+});
 
 // TODO 6.2: Create "/api/quote" route
-
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 
 // TODO 7: Start server using app.listen
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
